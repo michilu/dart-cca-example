a2a44555d7767d661418802e842df8a7f4d2bf4f
ENDOH takanao <djmchl@gmail.com>
2014-08-22 18:36:20 +0800

    fix errors on Safari
    [Error] Missing initialization of polymer elements. Please check that the list of entry points in your pubspec.yaml is correct. If you are using pub-serve, you may need to restart it.
    	X1 (main.dart.precompiled.js, line 26424)
    	Ok (main.dart.precompiled.js, line 26416)
    	(anonymous function) (main.dart.precompiled.js, line 25449)
    	(anonymous function) (main.dart.precompiled.js, line 892)
    	RqO (main.dart.precompiled.js, line 676)
    	(anonymous function) (main.dart.precompiled.js, line 32474)
    	(anonymous function) (main.dart.precompiled.js, line 32474)
    	onLoad (main.dart.precompiled.js, line 32473)
    [Error] TypeError: 'undefined' is not an object (evaluating 'fYK.prototype.grZK=function(receiver){return receiver.alt}') (main.dart.precompiled.js, line 32664)
    [Error] NoSuchMethodError : method not found: 'Symbol("main")' (index.html_bootstrap.dart.precompiled.js, line 1424)
    Receiver: ""
    Arguments: []

--- web/my_element.dart
+++
@@ -1,9 +0,0 @@
-import 'package:polymer/polymer.dart'; // I want to use show here,
-                                       // bug http://dartbug.com/16555
-
-@CustomTag('my-element')
-class MyElement extends PolymerElement {
-  @published String message = 'bindings are neat!';
-
-  MyElement.created() : super.created();
-}
